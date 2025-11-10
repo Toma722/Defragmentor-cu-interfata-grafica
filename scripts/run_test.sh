b91758e2ab7309428a17a01e7574752563711ae2
Toma <toma.jianu@gmail.com>
2025-11-10 10:10:12 +0200

    rezolvat run_test.sh(cred?)

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -4,10 +4,10 @@ startup_sleep=$1
 command_sleep=$2
 exit_sleep=$3
 
-APP_WINDOW=${APP_WINDOW:-"My Window"}
+APP_WINDOW=${APP_WINDOW:-"Defragmentor v0.3"}
 
-#sleep "${startup_sleep}" && xdotool search --name "${APP_WINDOW}" key X &&
-#sleep "${command_sleep}" && xdotool search --name "${APP_WINDOW}" key Y &&
-#sleep "${command_sleep}" && xdotool search --name "${APP_WINDOW}" key X &&
-#sleep "${command_sleep}" && xdotool search --name "${APP_WINDOW}" key Z &&
-#sleep "${exit_sleep}" && xdotool search --name "${APP_WINDOW}" key Alt+F4
+sleep "${startup_sleep}" && xdotool search --name "${APP_WINDOW}" key X &&
+sleep "${command_sleep}" && xdotool search --name "${APP_WINDOW}" key Y &&
+sleep "${command_sleep}" && xdotool search --name "${APP_WINDOW}" key X &&
+sleep "${command_sleep}" && xdotool search --name "${APP_WINDOW}" key Z &&
+sleep "${exit_sleep}" && xdotool search --name "${APP_WINDOW}" key Alt+F4
